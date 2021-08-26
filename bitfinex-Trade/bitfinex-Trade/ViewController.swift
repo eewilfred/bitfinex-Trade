7c70169ff15beade3da86ed4cd9a8cf92e337f8b
Edwin Wilson <ewilson@altayer.com>
2021-08-26 12:51:19 +0400

    Add initial project

---
+++ bitfinex-Trade/bitfinex-Trade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitfinex-Trade
+//
+//  Created by Edwin Wilson on 26/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bitfinex-Trade/bitfinex-TradeTests/bitfinex_TradeTests.swift
@@ -0,0 +1,33 @@
+//
+//  bitfinex_TradeTests.swift
+//  bitfinex-TradeTests
+//
+//  Created by Edwin Wilson on 26/08/2021.
+//
+
+import XCTest
+@testable import bitfinex_Trade
+
+class bitfinex_TradeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
