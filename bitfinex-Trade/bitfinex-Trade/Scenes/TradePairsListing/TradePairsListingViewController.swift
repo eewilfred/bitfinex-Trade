146c6d113a7f2232e92015e77ff7c640bacc0169
Edwin Wilson <ewilson@altayer.com>
2021-08-28 21:08:31 +0400

    Update trade pair listing vc with presentation and viewmodel
    - Updated storyboard
    - Add fetch pair from vc via view model logic

--- bitfinex-Trade/bitfinex-Trade/Scenes/TradePairsListing/TradePairsListingViewController.swift
+++ bitfinex-Trade/bitfinex-Trade/Scenes/TradePairsListing/TradePairsListingViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class TradePairsListingViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+
+    var model = TradePairsListingViewModel()
+    var presentation = TradePairsListingPresentation()
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        model.fetchTradePairs { success in
+            // TODO:
+        }
     }
 }
 
