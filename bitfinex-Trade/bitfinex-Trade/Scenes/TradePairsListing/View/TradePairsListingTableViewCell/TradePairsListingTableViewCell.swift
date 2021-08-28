ab860812602c9d6504dabb90ceb6e88450bcef6d
Edwin Wilson <ewilson@altayer.com>
2021-08-28 23:17:32 +0400

    Update trade pairs list cell ui
    - Added mapping ui with presentation for updates

--- bitfinex-Trade/bitfinex-Trade/Scenes/TradePairsListing/View/TradePairsListingTableViewCell/TradePairsListingTableViewCell.swift
+++ bitfinex-Trade/bitfinex-Trade/Scenes/TradePairsListing/View/TradePairsListingTableViewCell/TradePairsListingTableViewCell.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class TradePairsListingTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var symbolLabel: UILabel!
+    @IBOutlet private weak var lastPriceLabel: UILabel!
+    @IBOutlet private weak var dailyChange: UILabel!
+
+    var presentation: TradePairsListingCellPresentation? {
+        didSet {
+            updateUI()
+        }
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        dailyChange.layer.borderColor = UIColor.black.cgColor
+        dailyChange.layer.borderWidth = 1.0
+        dailyChange.layer.cornerRadius = 5.0
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {}
+
+    private func updateUI() {
+
+        guard let presentation = presentation else {
+            return
+        }
+
+        symbolLabel.text = presentation.symbol
+        lastPriceLabel.text = presentation.lastPrice
+        dailyChange.attributedText = presentation.dailyChange
+        dailyChange.backgroundColor = presentation.dailyChangeBgColor
+    }
 }
