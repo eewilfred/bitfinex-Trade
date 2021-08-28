5988825f6398ee7ac1806786038ab5a6b61e1a71
Edwin Wilson <ewilson@altayer.com>
2021-08-28 21:05:28 +0400

    Add initial files for trading pair table view cell

---
+++ bitfinex-Trade/bitfinex-Trade/Scenes/TradePairsListing/View/TradePairsListingTableViewCell/TradePairsListingCellPresentation.swift
@@ -0,0 +1,10 @@
+//
+//  TradePairsListingCellPresentation.swift
+//  bitfinex-Trade
+//
+//  Created by Edwin Wilson on 28/08/2021.
+//
+
+import Foundation
+
+struct TradePairsListingCellPresentation: Hashable {}
---
+++ bitfinex-Trade/bitfinex-Trade/Scenes/TradePairsListing/View/TradePairsListingTableViewCell/TradePairsListingTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TradePairsListingTableViewCell.swift
+//  bitfinex-Trade
+//
+//  Created by Edwin Wilson on 28/08/2021.
+//
+
+import UIKit
+
+class TradePairsListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {}
+}
