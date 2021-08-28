8db826d3669778df5a0fd2929a643f8426c628ec
Edwin Wilson <ewilson@altayer.com>
2021-08-28 21:28:06 +0400

    Add table view cell extenstion

---
+++ bitfinex-Trade/bitfinex-Trade/Extenstions/UITableViewCell+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewCell+Extension.swift
+//  bitfinex-Trade
+//
+//  Created by Edwin Wilson on 28/08/2021.
+//
+
+import UIKit
+
+extension UITableViewCell {
+
+    /// Class name as cell identifier string
+    class var identifier: String { return String(describing: self) }
+
+    /// Initialize xib using Class name
+    class var nib: UINib { return UINib(nibName: identifier, bundle: nil) }
+}
