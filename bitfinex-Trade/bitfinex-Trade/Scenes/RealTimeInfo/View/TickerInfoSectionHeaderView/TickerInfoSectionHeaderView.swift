804aca5474f80b5ddec8c9cd21728dec6066ea9d
Edwin Wilson <ewilson@altayer.com>
2021-08-31 01:22:29 +0400

    Add collection view header cells for ticker and trade sections

---
+++ bitfinex-Trade/bitfinex-Trade/Scenes/RealTimeInfo/View/TickerInfoSectionHeaderView/TickerInfoSectionHeaderView.swift
@@ -0,0 +1,10 @@
+//
+//  TickerInfoSectionHeaderView.swift
+//  bitfinex-Trade
+//
+//  Created by Edwin Wilson on 31/08/2021.
+//
+
+import UIKit
+
+class TickerInfoSectionHeaderView: UICollectionReusableView {}
---
+++ bitfinex-Trade/bitfinex-Trade/Scenes/RealTimeInfo/View/TradeInfoSectionHeaderView/TradeInfoSectionHeaderView.swift
@@ -0,0 +1,10 @@
+//
+//  TradeInfoSectionHeaderView.swift
+//  bitfinex-Trade
+//
+//  Created by Edwin Wilson on 31/08/2021.
+//
+
+import UIKit
+
+class TradeInfoSectionHeaderView: UICollectionReusableView {}
