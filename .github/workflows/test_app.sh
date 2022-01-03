7dc89466e80b2e37f0c8afb3ea3a2952562fe003
Edwin Wilson <edwin33wilson@gmail.com>
2022-01-03 10:12:57 +0400

    Update test_app.sh

--- .github/workflows/test_app.sh
+++ .github/workflows/test_app.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -eo pipefail
+cd bitfinex-Trade/bitfinex-Trade/
 
-xcodebuild -workspace Calculator.xcworkspace \
-            -scheme Calculator\ iOS \
+xcodebuild -workspace bitfinex-Trade.xcworkspace \
+            -scheme bitfinex-Trade\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
-            clean test | xcpretty
+            clean test
